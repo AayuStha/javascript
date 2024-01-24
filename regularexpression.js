9699df4810d120b4d83d41d6d0f9d98b42f4015c
AayuStha <aayustha21@gmail.com>
2024-01-24 07:22:24 +0545

    Unit 1

--- regularexpression.js
+++ regularexpression.js
@@ -2,6 +2,7 @@ var str = "Hello World";
 var n = str.search(/world/i); 
 
 var str = "Hello World!";
-var res = str.replace(/[a]/i, "Worlds");
+var res = str.replace(/world/i, "Worlds");
 
 document.write(n);
+document.write(res);
