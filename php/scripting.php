59085ea8664ce475e36820c54d9ea7301794e1f0
AayuStha <aayustha21@gmail.com>
2024-02-13 10:13:42 +0545

    added php

---
+++ php/scripting.php
@@ -0,0 +1,48 @@
+<?php
+    // $season = array("summer", "winter", "spring", "autumn");
+    // foreach($season as $arr){
+    //     echo"Season is: $arr \n";
+    // }
+    
+    // $n =1;
+    // while($n <=10):
+    //     echo"$n \n";
+    //     $n++;
+    // endwhile;
+
+    // $n =1;
+    // do{
+    //     echo"$n \n";
+    //     $n++;
+    // }while($n<=10);
+    
+    //ARRAYS
+
+    //indexed array
+
+    // $season = array("summer", "winter", "spring", "autumn");
+    // echo"Season are: $season[0], $season[1], $season[2], $season[3] \n";
+
+    //associative array
+
+    // $age=array("Manish"=>"35", "Anish"=>"45", "Hari"=>"20");
+    // foreach($age as $x =>$x_value){
+    //     echo"Key= " . $x. ",Value= " .$x_value;
+    //     echo"\n";
+    // }
+    
+    //multidimensional array
+    // $emp = array(
+    //     array(1, "Ram", 400000),
+    //     array(2,"shyam", 500000),
+    //     array(3,"Hari", 300000)
+    // );
+    
+    // for($row = 0; $row <3; $row++){
+    //     for($col = 0; $col <3;$col++){
+    //         echo $emp[$row][$col]." ";
+    //     }
+    //     echo"\n";
+    // }
+?>
+
--- regularexpression.js
+++ regularexpression.js
@@ -2,7 +2,28 @@ var str = "Hello World";
 var n = str.search(/world/i); 
 
 var str = "Hello World!";
-var res = str.replace(/world/i, "Worlds");
+var res = str.replace(/[o]/i, "Worlds");
 
-document.write(n);
-document.write(res);
+var str = "Hello World!";
+var red = str.search(/\s/i);
+
+// document.write(n);
+// document.write(res);
+// document.write(red);
+
+function myfunction(){
+    var str = "Helloo World! Hello javascript";
+    var patt1 = /lo*/g; //returns looo,l,lo
+    var result= str.match(patt1);
+    document.getElementById("demo").innerHTML = result;
+}
+
+function test1(){
+    // var test = /a/.test("Helloo World! Hello javascript"); //returns true
+    // return true;
+    var tes = /a/.exec("Helloo World! Hello javascript"); 
+    return tes
+
+}
+document.write(test1());
+
